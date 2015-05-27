bf316bcd8cc8d45655377707cae5e26a28b6d8e7
Chris McKenzie <kristopolous@yahoo.com>
2015-05-27 16:46:32 -0700

    Adding a with to simplify lambda maps

--- db.js
+++ db.js
@@ -1136,7 +1136,7 @@
 
         eval( "keyer = function(r,ref){try{ref[rX] = update[rX] = r;} catch(x){}}".replace(/X/g, field));
       } else {
-        eval( "keyer = function(r,ref){var val = X;try{ref[val] = update[val] = r;} catch(x){}}".replace(/X/g, field));
+        eval( "keyer = function(r,ref){with(r) { var val = X };try{ref[val] = update[val] = r;} catch(x){}}".replace(/X/g, field));
       }
 
       function update(whence) {
