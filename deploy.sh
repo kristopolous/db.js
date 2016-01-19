f8f048120f4c0ee829c2c669e53215073e59f95a
Chris McKenzie <kristopolous@yahoo.com>
2016-01-18 16:18:53 -0800

    Fixing a bug in the deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ for file in db db-helper; do
   cp src/$file.js $file.js
 done
 
-echo "DB.__version__='`git describe`';" >> $file.js
+echo "DB.__version__='`git describe`';" >> db.js
 
 for file in db db-helper; do
   ./minifier.sh $file
