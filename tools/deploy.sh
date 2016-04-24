d6631ae2c88168b1ca1f0c3a242d32113a620451
Chris McKenzie <kristopolous@yahoo.com>
2016-04-23 23:13:40 -0700

    Updating the versioning scheme

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,10 +4,14 @@ for file in db db-helper; do
   cp src/$file.js release/$file.js
 done
 
-echo "DB.__version__='`git describe`';" >> release/db.js
+version=`git describe | awk -F '-' ' { print $1"."$(NF-1) } '`
+age=`date +%Y%m%d`
+fullversion=$version-$age
+echo "DB.version='$fullversion';" >> release/db.js
 
 cd release
 for file in db db-helper; do
   ../tools/minifier.sh $file
 done
 
+echo $fullversion
